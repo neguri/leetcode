a62967667dbbfcc4af87b4b50571c128c2fb574a
elliott.choi <elliott.choi@lge.com>
2017-08-25 13:47:36 +0900

    refactoring

--- 344_Reserse_string.cpp
+++ 344_Reserse_string.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     string reverseString(string s) {
         int len = s.length();
@@ -7,5 +7,19 @@ public:
           r.append(1,s[len-1-i]);  
         }
         return r;
+    }
+};
+*/
+
+//without additional memory
+
+class Solution {
+public:
+    string reverseString(string s) {
+        int len = s.length();
+        for(int i = 0; i<len/2;i++) {
+         swap(s[i],s[len-1-i]); 
+        }
+        return s;
     }
 };
