731ca86ca7119955f4a7d175023825a98b3611c9
neguri <projekct@gmail.com>
2017-10-11 00:14:04 +0900

    refactoring

--- easy/121_Best_time_to_buy_.cpp
+++ easy/121_Best_time_to_buy_.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int max = 0;
+        int max = 0; 
+		int min_idx = 0;
+		int max_idx = 0;
+
+		for(int i = 0; i< prices.size(); i++) {
+
+			if(prices[i] > prices[max_idx]) {
+				max_idx = i;
+				int delta = prices[max_idx] - prices[min_idx];
+				if(delta > max) max = delta;
+			}
+			if(prices[i] < prices[min_idx]) max_idx = min_idx = i;
+
+
+	/*
         for(int i = 0; i<prices.size();i++) {
             for(int j = i+1; j<prices.size();j++){
                 
@@ -11,7 +25,9 @@ public:
                 
             } 
         }
-        
+
+	*/
+
         return max;
     }
 };
