37ae426938746ddd152e564da6f8233beb0a20d0
neguri <projekct@gmail.com>
2017-11-02 00:44:53 +0900

    refactoring

--- easy/58_length_of_last_word.cpp
+++ easy/58_length_of_last_word.cpp
@@ -40,3 +40,44 @@ public:
         return cnt;
     }
 };
+
+/*
+//before refactoring
+
+class Solution {
+public:
+    
+    void removeLastSpace(string& s) {
+        
+        int sz = s.length();
+        for(int i = 0; i<sz;i++) {
+            if(s[sz-i-1] == ' ')
+                s.pop_back();
+            else
+                return;
+        }
+        
+    }
+    
+    int lengthOfLastWord(string s) {
+        
+        removeLastSpace(s);
+        
+        int sz = s.length();
+        int cnt = 0;
+        
+        
+        
+        for(int i = 0; i<sz; i++) {
+            
+            if(s[sz-i-1] != ' ') 
+                cnt++;
+            else 
+                break;
+        }
+        
+        return cnt;
+    }
+};
+
+*/
