c7707bbb6f568435a6b87a78ab10f8c20f76ce11
neguri <projekct@gmail.com>
2017-11-09 23:32:23 +0900

    fix for accept condition. but it tooks 15 ms

--- easy/415_Add_String.cpp
+++ easy/415_Add_String.cpp
@@ -1,6 +1,79 @@
 class Solution {
 public:
     
+   
+    string addStrings(string num1, string num2) {
+        
+    string num3;
+    vector<char> v;
+
+
+    size_t sz1 = num1.length();
+    size_t sz2 = num2.length();
+    
+    int i = 0;
+    int sum;
+    int carry = 0;
+
+
+    while( sz1 > 0 && sz2 > 0) {
+
+       sum = (num1[sz1-1] - '0') + (num2[sz2-1] - '0') + carry;
+       if(sum > 9) { carry = 1; sum -=10; }  else carry = 0;
+
+       v.push_back(sum + '0');
+       //cout<<sum;
+       sz1--;
+       sz2--;
+
+    }
+
+
+    if(sz1 > 0) {
+
+        while( sz1 > 0) {
+          sum = num1[sz1-1] -'0' + carry;   
+          if(sum > 9) { carry = 1; sum -=10; }  else carry = 0;
+            v.push_back(sum + '0');
+           // cout<<sum;
+          sz1--;
+        }
+    }else if (sz2 > 0){
+
+        while( sz2 > 0) {
+          sum = num2[sz2-1] -'0' + carry;   
+
+          cout<<"debug" <<sz2<<" "<<sum<<endl;
+            
+          if(sum > 9) { carry = 1; sum -=10; }  else carry = 0;
+          v.push_back(sum + '0');
+           // cout<<sum;
+            sz2--;
+        }
+
+    } 
+    
+    if(carry == 1) v.push_back('1');
+    //cout<<sum;
+
+    size_t sz = v.size();
+    for(int i = 0; i < sz; i++) {
+
+     num3.push_back(v[sz-1-i]);
+
+    }
+        
+    return num3;
+
+    }
+};
+
+/*
+below is big number problem
+
+class Solution {
+public:
+    
     unsigned long long convToInt(string str) {
         
         unsigned long long r = str[0] - '0';
@@ -46,3 +119,5 @@ public:
         return _ret;
     }
 };
+
+*/
