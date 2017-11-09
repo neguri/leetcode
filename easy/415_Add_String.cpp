3c0357c57ac368e351515fdc568f0b4df3883ead
neguri <projekct@gmail.com>
2017-11-10 00:08:01 +0900

    runtime 12 ms

--- easy/415_Add_String.cpp
+++ easy/415_Add_String.cpp
@@ -14,6 +14,77 @@ public:
     int i = 0;
     int sum;
     int carry = 0;
+    char c;
+
+
+    while( sz1 > 0 && sz2 > 0) {
+
+       sum = (num1[sz1-1] - '0') + (num2[sz2-1] - '0') + carry;
+       if(sum > 9) { carry = 1; sum -=10; }  else carry = 0;
+
+       c = sum + '0';
+       num3.insert(0, &c);
+
+       sz1--;
+       sz2--;
+
+    }
+
+
+    if(sz1 > 0) {
+
+        while( sz1 > 0) {
+          sum = num1[sz1-1] -'0' + carry;   
+          if(sum > 9) { carry = 1; sum -=10; }  else carry = 0;
+            c = sum+'0';
+            num3.insert(0,&c);
+
+          sz1--;
+        }
+    }else if (sz2 > 0){
+
+        while( sz2 > 0) {
+          sum = num2[sz2-1] -'0' + carry;   
+
+          if(sum > 9) { carry = 1; sum -=10; }  else carry = 0;
+
+          c = sum + '0';
+          num3.insert(0, &c);
+
+          sz2--;
+        }
+
+    } 
+    
+
+    c = '1';
+    if(carry == 1) num3.insert(0, &c);
+
+    return num3;
+
+    }
+};
+
+/*
+
+
+
+class Solution {
+public:
+    
+   
+    string addStrings(string num1, string num2) {
+        
+    string num3;
+    vector<char> v;
+
+
+    size_t sz1 = num1.length();
+    size_t sz2 = num2.length();
+    
+    int i = 0;
+    int sum;
+    int carry = 0;
 
 
     while( sz1 > 0 && sz2 > 0) {
@@ -67,7 +138,7 @@ public:
 
     }
 };
-
+*/
 /*
 below is big number problem
 
